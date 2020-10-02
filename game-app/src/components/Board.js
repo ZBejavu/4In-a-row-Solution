dc05798df90424890805c4bc95bf80ffc7b41f5b
ZBejavu <67857471+ZBejavu@users.noreply.github.com>
2020-10-02 12:39:57 +0300

    board turns work

--- game-app/src/components/Board.js
+++ game-app/src/components/Board.js
@@ -1,31 +1,51 @@
 import React, {useState,useEffect} from 'react';
 function Board() {
     const [board, setBoard] = useState();
+    const [turn, setTurn] = useState(1);
     useEffect(() => {
         const gameBoard = [];
         for(let i =0; i< 7; i++){
             gameBoard[i] = [];
             for(let j =0; j<6; j++){
-                gameBoard[i][j] = j;
+                gameBoard[i][j] = 0;
             }
         }
         console.log(gameBoard);
         setBoard(gameBoard);
     },[])
 
+    function insertTurn(i){
+        const myIndex = board[i].findIndex(value => value===0);
+        console.log(myIndex);
+        if(myIndex === -1){
+            return;
+        }
+        const newBoard = board.slice();
+        if(turn%2 === 0){
+            newBoard[i][myIndex] = 1;
+            setTurn(turn+1);
+        } else{
+            newBoard[i][myIndex] = 2;
+            setTurn(turn+1);
+        }
+        console.log(newBoard);
+        setBoard(newBoard);
+    }
   return (
     <div className="Board">
                 {
             !board? null :
             board.map((collumn, i) => {
-                return <div className="Collumn" id={`collumn${i}`}>
+                return <div 
+                onClick={() => { insertTurn(i)}}
+                className="Collumn" id={`collumn${i}`}>
                     {
                         collumn.map((square, index) => {
                             return <div className="Square" id={`square${index}`}>
                                 {
-                                    // board[i][index] ===0? square 
-                                    // :
-                                    <div className={board[i][index] === 1? 'player1':'player2'}>{square}</div>
+                                    square === 0 ? null 
+                                    :
+                                    <div className={square === 1? 'player1':'player2'}></div>
                                 }
                             </div>
                         })
--- game-app/src/components/Square.js
+++ game-app/src/components/Square.js
@@ -1,40 +1,14 @@
 import React, {useState,useEffect} from 'react';
-function Board() {
-    const [board, setBoard] = useState();
+function Square({cloumnClick}) {
+    const [square, setSquare] = useState();
     useEffect(() => {
-        const gameBoard = [];
-        for(let i =0; i< 7; i++){
-            gameBoard[i] = [];
-            for(let j =0; j<6; j++){
-                gameBoard[i][j] = 0;
-            }
-        }
-        console.log(gameBoard);
-        setBoard(gameBoard);
+       
     },[])
 
   return (
-    <div className="Board">
-        {
-            !board? null :
-            board.map((collumn, i) => {
-                return <div className="Collumn" id={`collumn${i}`}>
-                    {
-                        collumn.map((square, index) => {
-                            return <div className="Square" id={`square${index}`}>
-                                {
-                                    board[i][index] ===0? square 
-                                    :
-                                    <div className={board[i][index] === 1? 'player1':'player2'}></div>
-                                }
-                            </div>
-                        })
-                    }
-                </div>
-            })
-        }
+    <div className="Square">
     </div>
   );
 }
 
-export default Board;
+export default Square;
